df54483a95eb530c5faf24bedf5898be51329e6d
LewisHillard-Q <18LJH@queensu.ca>
2019-11-11 17:29:49 -0500

    reoganizing

--- File Manipulation/RemoveDups.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-lines_seen = set() # holds lines already seen
-outfile = open("out.txt", "a+")
-for line in open(sys.argv[1], "r"):
-    if line not in lines_seen: # not a duplicate
-        outfile.write(line)
-        lines_seen.add(line)
-outfile.close()
